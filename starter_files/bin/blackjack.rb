b50bb929e52097093a13dbfd93fbb8b443f4c9eb
lifeprolific <thegregoman@gmail.com>
2018-08-17 10:34:44 -0400

    changed blackjack.rb file use and cleaned up

--- starter_files/bin/blackjack.rb
+++ starter_files/bin/blackjack.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
-require_relative '../blackjack'
+require_relative '../lib/blackjack_game'
+require_relative '../lib/player'
 
-BlackjackGame.new
+puts "Hello and welcome to the game of blackjack! Let's begin."
+game = BlackjackGame.new
+while game.player.bank > 0
+  game.new_round
+  game.play_hand
+end
+puts "You have run out of money. Thank you for playing blackjack!"
