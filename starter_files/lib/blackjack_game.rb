ac58a5153e48d811a78e4163c92dc4d55797b991
lifeprolific <thegregoman@gmail.com>
2018-08-17 14:01:48 -0400

    completed basic game loop

--- starter_files/lib/blackjack_game.rb
+++ starter_files/lib/blackjack_game.rb
@@ -16,6 +16,7 @@ class BlackjackGame
 
   def new_round
     @player.hand.clear
+    @dealer.hand.clear
     puts "You have $#{@player.bank} and bet $10."
     @player.bank -= 10
     puts "You now have $#{@player.bank}."
@@ -52,13 +53,13 @@ class BlackjackGame
     # loop until stay or bust
     while (hit && !bust)
       puts "Your current hand: #{@player.hand}."
-      puts "Your hand's value: #{player.hand.valid_values}"
+      puts "Your hand's value: #{@player.hand.valid_values}"
       puts "The house's current hand: #{@dealer.hand.cards[0]}, face-down card"
       hit = check_hit
       if hit
-        deal_card(player)
+        deal_card(@player)
       end
-      bust = player.hand.valid_values.length == 0
+      bust = @player.hand.valid_values.length == 0
       if bust
         puts "You bust!"
       end
@@ -67,11 +68,53 @@ class BlackjackGame
   end
 
   def house_turn
-    puts "house_turn"
+    bust = false
+    stay = !(@dealer.hand.valid_values.max < 17)
+    puts "the house's hand: #{@dealer.hand}"
+    puts "the house hand's value = #{@dealer.hand.valid_values}"
+    while (!bust) && (!stay)
+      card = @deck.draw
+      puts "the house hits, dealing itself the #{card}"
+      @dealer.hand.add_card(card)
+      puts "the house hand's value = #{@dealer.hand.valid_values}"
+      if @dealer.hand.valid_values.length == 0
+        bust = true
+        puts "The house busts!"
+      elsif @dealer.hand.valid_values.max >= 17
+        stay = true
+      end
+    end
+    if stay
+      puts "The house stays."
+    end
   end
 
   def resolve_hands
-    puts "resolve_hands"
+    player_score = resolve_score(@player)
+    dealer_score = resolve_score(@dealer)
+    puts "Your hand's value: #{player_score}"
+    puts "The house hand's value: #{dealer_score}"
+    if player_score == "bust"
+      puts "You lose."
+    elsif dealer_score == "bust"
+      puts "You win $20!"
+      @player.bank = @player.bank + 20
+    elsif player_score > dealer_score
+      puts "You win $20!"
+      @player.bank = @player.bank + 20
+    else
+      puts "You lose."
+    end
+  end
+
+  def resolve_score(player)
+    player_score = player.hand.valid_values
+    if player_score.length == 0
+      player_score = "bust"
+    else
+      player_score = player_score.max
+    end
+    player_score
   end
 
   def check_hit
